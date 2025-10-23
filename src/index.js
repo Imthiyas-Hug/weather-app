2f5fb22c436dca7fa38f386ef829ba9168da98dd
Imthiyas-Hug <imthiyashugi@gmail.com>
2025-10-23 22:19:48 +0530

    modify files

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import "./styles.css";
+const searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
+const centerDiv = document.querySelector(".center-part");
+const bottomDiv = document.querySelector(".bottom-part-div");
+const city = document.querySelector("#city");
+const weatherIcon = document.querySelector(".weather-icon");
+const temp = document.querySelector("#temperature");
+const humidityValue = document.querySelector("#humidity-value");
+const windValue = document.querySelector("#wind-value");
+const toggleBtn = document.querySelector(".toggle-btn");
+const loading = document.querySelector("#loading");
+const weatherDataDiv = document.querySelector(".weather-data");
+async function getWeatherData() {
+  try {
+    const cityValue = searchInput.value;
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityValue}?unitGroup=us&key=UXA9C2SB49MAYTRTEM4T9ZVBV&contentType=json`
+    );
+    const weatherData = await response.json();
+    loading.style.display = "none";
+    centerDiv.style.display = "flex";
+    bottomDiv.style.display = "flex";
+    toggleBtn.style.display = "block";
+    const imageURL = await import(`./images/${weatherData.days[0].icon}.png`);
+    weatherIcon.src = imageURL.default;
+    city.textContent = weatherData.address;
+    temp.textContent = `${weatherData.days[0].temp}°F`;
+    humidityValue.textContent = `${weatherData.days[0].humidity}%`;
+    windValue.textContent = `${(
+      weatherData.days[0].windspeed * 1.60934
+    ).toFixed(2)} km/hr`;
+    toggleBtn.addEventListener("click", (e) => {
+      if (e.target.textContent == "°C") {
+        let celcius = (((weatherData.days[0].temp - 32) * 5) / 9).toFixed(2);
+        console.log("cel", celcius);
+        temp.innerText = `${celcius}°C`;
+        e.target.textContent = "°F";
+      } else {
+        let fahrenheit = `${weatherData.days[0].temp}°F`;
+        console.log("fah", fahrenheit);
+        temp.textContent = fahrenheit;
+        e.target.textContent = "°C";
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  loading.style.display = "block";
+  centerDiv.style.display = "none";
+  bottomDiv.style.display = "none";
+  toggleBtn.style.display = "none";
+  getWeatherData();
+});
